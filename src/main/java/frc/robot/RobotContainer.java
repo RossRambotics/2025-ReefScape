796de3af5e5c145262bb4965dd7092c02c72aebc
hollisms <info@spyschuler.com>
2025-01-19 23:49:57 -0500

    added simulation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,13 +17,23 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Mechanism;
 import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.ArmBase;
+import frc.robot.subsystems.ArmExtension;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.Wrist;
 
 public class RobotContainer {
+    // Subsystems
+    final static public ArmBase m_armBase = new ArmBase();
+    final static public ArmExtension m_armExtension = new ArmExtension();
+    final static public Wrist m_wrist = new Wrist();
+    final static public Mechanisms m_mechanisms = new Mechanisms();
+
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second
+                                                                                      // max angular velocity
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
@@ -54,32 +64,32 @@ public class RobotContainer {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-            )
-        );
+                // Drivetrain will execute this command periodically
+                drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with
+                                                                                                   // negative Y
+                                                                                                   // (forward)
+                        .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                        .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with
+                                                                                    // negative X (left)
+                ));
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
+        joystick.a().whileTrue(RobotContainer.m_armBase.getSetGoalCommand(90.0));
+        joystick.b().whileTrue(RobotContainer.m_armBase.getSetGoalCommand(0.0));
+        joystick.x().whileTrue(RobotContainer.m_armExtension.getSetGoalCommand(2.0));
+        joystick.y().whileTrue(RobotContainer.m_armExtension.getSetGoalCommand(0.0));
+        joystick.button(5).whileTrue(RobotContainer.m_wrist.getSetGoalCommand(40.0));
+        joystick.button(6).whileTrue(RobotContainer.m_wrist.getSetGoalCommand(0.0));
 
-        joystick.pov(0).whileTrue(drivetrain.applyRequest(() ->
-            forwardStraight.withVelocityX(0.5).withVelocityY(0))
-        );
-        joystick.pov(180).whileTrue(drivetrain.applyRequest(() ->
-            forwardStraight.withVelocityX(-0.5).withVelocityY(0))
-        );
+        joystick.pov(0).whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(0)));
+        joystick.pov(180)
+                .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        // joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        // joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        // joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        // joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // reset the field-centric heading on left bumper press
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
---
+++ src/main/java/frc/robot/subsystems/ArmBase.java
@@ -0,0 +1,135 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.controls.Follower;
+
+import frc.robot.RobotContainer;
+import frc.robot.sim.PhysicsSim;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static edu.wpi.first.units.Units.*;
+
+public class ArmBase extends SubsystemBase {
+    final TalonFX m_LeftMotor = new TalonFX(30, "rio");
+    final TalonFX m_RightMotor = new TalonFX(31, "rio");
+
+    private final MotionMagicVoltage m_mmReq = new MotionMagicVoltage(0);
+    private GenericEntry m_GE_PID_kS = null;
+    private GenericEntry m_GE_PID_kV = null;
+    private GenericEntry m_GE_PID_kA = null;
+    private GenericEntry m_GE_PID_kP = null;
+    private GenericEntry m_GE_PID_kI = null;
+    private GenericEntry m_GE_PID_kD = null;
+    private GenericEntry m_GE_bUpdatePID = null;
+    private GenericEntry m_GE_Position = null;
+    private GenericEntry m_GE_Velocity = null;
+    private GenericEntry m_GE_Goal = null;
+
+    /** Creates a new ArmPivot. */
+    public ArmBase() {
+        TalonFXConfiguration cfg = new TalonFXConfiguration();
+
+        // Configure the right motor to follow the left motor (but opposite direction)
+        m_RightMotor.setControl(new Follower(m_LeftMotor.getDeviceID(), true));
+
+        /* Configure gear ratio */
+        FeedbackConfigs fdb = cfg.Feedback;
+        fdb.SensorToMechanismRatio = 1.0; // TODO: Calibrate motor rotations to sensor degrees
+
+        /* Configure Motion Magic */
+        MotionMagicConfigs mm = cfg.MotionMagic;
+        mm.withMotionMagicCruiseVelocity(RotationsPerSecond.of(10)) // 5 (mechanism) rotations per second cruise
+                .withMotionMagicAcceleration(RotationsPerSecondPerSecond.of(50)) // Take approximately 0.5 seconds to
+                                                                                 // reach max
+                                                                                 // vel
+                // Take approximately 0.1 seconds to reach max accel
+                .withMotionMagicJerk(RotationsPerSecondPerSecond.per(Second).of(100));
+
+        Slot0Configs slot0 = cfg.Slot0;
+        slot0.GravityType = GravityTypeValue.Arm_Cosine;
+        slot0.kS = 0.25; // Add 0.25 V output to overcome static friction
+        slot0.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+        slot0.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+        slot0.kP = 5; // A position error of 0.2 rotations results in 12 V output
+        slot0.kI = 0; // No output for integrated error
+        slot0.kD = 1.0; // A velocity error of 1 rps results in 0.5 V output
+
+        m_GE_PID_kS = Shuffleboard.getTab("ArmBase").add("ArmBase_kS", slot0.kS).getEntry();
+        m_GE_PID_kV = Shuffleboard.getTab("ArmBase").add("ArmBase_kV", slot0.kV).getEntry();
+        m_GE_PID_kA = Shuffleboard.getTab("ArmBase").add("ArmBase_kA", slot0.kA).getEntry();
+        m_GE_PID_kP = Shuffleboard.getTab("ArmBase").add("ArmBase_kP", slot0.kP).getEntry();
+        m_GE_PID_kI = Shuffleboard.getTab("ArmBase").add("ArmBase_kI", slot0.kI).getEntry();
+        m_GE_PID_kD = Shuffleboard.getTab("ArmBase").add("ArmBase_kD", slot0.kD).getEntry();
+        m_GE_bUpdatePID = Shuffleboard.getTab("ArmBase").add("ArmBase_UpdatePID", false).getEntry();
+        m_GE_Position = Shuffleboard.getTab("ArmBase").add("ArmBase_Position", 0).getEntry();
+        m_GE_Velocity = Shuffleboard.getTab("ArmBase").add("ArmBase_Velocity", 0).getEntry();
+        m_GE_Goal = Shuffleboard.getTab("ArmBase").add("ArmBase_Goal", 0).getEntry();
+
+        StatusCode status = StatusCode.StatusCodeNotInitialized;
+        for (int i = 0; i < 5; ++i) {
+            status = m_LeftMotor.getConfigurator().apply(cfg);
+            if (status.isOK())
+                break;
+        }
+        if (!status.isOK()) {
+            System.out.println("Could not configure device. Error: " + status.toString());
+            // hi
+
+        }
+        this.setGoal(90);
+    }
+
+    private void setGoal(double degrees) {
+        m_LeftMotor.setControl(m_mmReq.withPosition(degrees / 360.0).withSlot(0));
+        m_GE_Goal.setDouble(degrees);
+    }
+
+    public Command getSetGoalCommand(double degrees) {
+        return this.runOnce(() -> setGoal(degrees));
+    }
+
+    @Override
+    public void periodic() {
+        // Update PID?
+        if (m_GE_bUpdatePID.getBoolean(false)) {
+            Slot0Configs slot0 = new Slot0Configs();
+            m_LeftMotor.getConfigurator().refresh(slot0);
+            slot0.kS = m_GE_PID_kS.getDouble(slot0.kS);
+            slot0.kV = m_GE_PID_kV.getDouble(slot0.kV);
+            slot0.kA = m_GE_PID_kA.getDouble(slot0.kA);
+            slot0.kP = m_GE_PID_kP.getDouble(slot0.kP);
+            slot0.kI = m_GE_PID_kI.getDouble(slot0.kI);
+            slot0.kD = m_GE_PID_kD.getDouble(slot0.kD);
+            m_LeftMotor.getConfigurator().apply(slot0);
+            m_GE_bUpdatePID.setBoolean(false);
+        }
+        // This method will be called once per scheduler run
+
+    }
+
+    public void simulationInit() {
+        PhysicsSim.getInstance().addTalonFX(m_LeftMotor, 0.1);
+    }
+
+    @Override
+    public void simulationPeriodic() {
+
+        m_GE_Velocity.setDouble(m_LeftMotor.getVelocity().getValueAsDouble());
+        m_GE_Position.setDouble(m_LeftMotor.getPosition().getValue().in(Degree));
+
+        RobotContainer.m_mechanisms.update(m_LeftMotor.getPosition(), m_LeftMotor.getVelocity());
+    }
+}
